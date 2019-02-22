dc28bededb44f955e8e5018f104812105e13d03d
Maxime De Clercq <5389859+TDarkShadow@users.noreply.github.com>
2019-02-22 18:45:55 +0100

    First version of adding PS1 as New File.

---
+++ ContextMenuNewPowershellScript/Add-PS1-ToNewContextMenu.ps1
@@ -0,0 +1,65 @@
+#requires -version 4
+<#
+.SYNOPSIS
+  Name: Add-PS1-ToNewContextMenu.ps1
+  This script adds 'PS Script' to the submenu 'New' of the contextmenu.
+.DESCRIPTION
+  This script adds a registry key to add the file type 'Powershell Script'
+  to the submenu 'New' of the contextmenu.
+.INPUTS
+  None
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         TDarkShadow
+  Creation Date:  2019-02-22
+  Purpose/Change: Initial script development
+#>
+
+# ---[Initialisations]---
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+# ---[Declarations]---
+
+$registryPath = "HKEY_CLASSES_ROOT\.ps1\ShellNew"
+
+$Name = "NullFile"
+
+$Value = ""
+
+# ---[Functions]---
+
+Function Add-PS1-ToNewContextMenu {
+  Begin {
+    Write-Host 'Adding Registry key for New PS Script.'
+  }
+  Process {
+    Try {
+    $registryPath = "Registry::" + $registryPath
+      If(!(Test-Path $registryPath)) {
+		New-Item -Path $registryPath -Force | Out-Null
+		Get-Item -Path $registryPath | New-ItemProperty -Name $name `
+			-PropertyType String -Force | Out-Null
+	} Else {
+		Get-Item -Path $registryPath | New-ItemProperty -Name $name `
+			-PropertyType String -Force | Out-Null
+	}
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed Successfully.'
+      Write-Host ' '
+    }
+  }
+}
+
+# ---[Execution]---
+Add-PS1-ToNewContextMenu
